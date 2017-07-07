1db28a40109de24bfbd17a37a1467b7418ab134f
Caitlin <ayashi@valefor.com>
2017-07-06 22:39:44 -0700

    adding some styles to the table

--- src/js/components/clothingTable.js
+++ src/js/components/clothingTable.js
@@ -2,10 +2,11 @@ var clothingTable = Vue.component("clothing-table", {
   template: `
   <div class="clothing-table">
     <category-select :categories="categories" @change="updateCategory"></category-select>
-    <table>
+    <table class="clothing-table__table">
       <thead>
         <tr>
           <th>Category</th>
+          <th>ID</th>
           <th>Clothing Name</th>
           <th>Hearts</th>
           <th>Styles</th>
@@ -15,20 +16,27 @@ var clothingTable = Vue.component("clothing-table", {
         </tr>
       </thead>
       <tbody>
-        <tr v-for="clothing in clothes">
-          <td>{{clothing.category}}</td>
-          <td>{{clothing.name}}</td>
-          <td>{{clothing.hearts}}</td>
-          <td>
-            <ul>
-              <li v-for="(style, index) in clothing.style">
+        <tr v-for="clothing in clothes" :id="clothing.category + '-' + clothing.id">
+          <td valign="top">{{clothing.category}}</td>
+          <td valign="top">{{clothing.id}}</td>
+          <td valign="top">{{clothing.name}}</td>
+          <td valign="top"><span v-for="heart in clothing.hearts">&hearts;</span></td>
+          <td valign="top">
+            <ul class="clothing-table__style-list style-list">
+              <li v-for="(style, index) in clothing.style" :class="'style-list__item style ' + 'style--' + index.toLowerCase()">
                 {{index}}: {{style.toUpperCase()}}
               </li>
             </ul>
           </td>
-          <td>{{clothing.tags}}</td>
-          <td>{{clothing.customizable}}</td>
-          <td><span v-for="(customization, index) in clothing.customizations">{{customization}}, </span></td>
+          <td valign="top">
+            <ul class="tags-list">
+              <li class="tags-list__item tag" v-for="(tag, index) in clothing.tags">
+                {{tag}}
+              </li>
+            </ul>
+          </td>
+          <td valign="top">{{clothing.customizable}}</td>
+          <td valign="top"><span v-for="(customization, index) in clothing.customizations">{{customization}}, </span></td>
         </tr>
       </tbody>
     </table>
--- src/js/nikki-form.js
+++ src/js/nikki-form.js
@@ -24,69 +24,11 @@ var app = new Vue({
     },
     orderedTags: function() {
       return store.state.tags.sort();
-    },
-    fullHearts: function() {
-      return this.clothingFormData.hearts >= 6;
-    },
-    reformatObject: function() {
-      var data = this.clothingFormData;
-      return {
-        id: data.id,
-        name: data.name,
-        hearts: data.hearts,
-        category: data.category,
-        style: data.ratings,
-        tags: data.tags,
-        customizable: data.customizable,
-        customizations: _.map(data.customizableItems, "id")
-      };
     }
   },
   methods: {
     updateStyleArray: function(value) {
       this.clothingFormData.clothingStyles = value;
-    },
-    selectedHeartsClass: function(num) {
-      return this.clothingFormData.hearts >= num
-        ? "form-group__heart--selected form-group__heart"
-        : "form-group__heart";
-    },
-    updateCustomItems: function(items) {
-      this.clothingFormData.customizableItems = items;
-    },
-    updateRatings: function(ratings) {
-      this.clothingFormData.ratings = ratings;
-    },
-    updateTags: function(tags) {
-      this.clothingFormData.tags = tags;
-    },
-    updateCustomizable: function(customizable) {
-      this.clothingFormData.customizable = customizable;
-    },
-    updateCategory: function(category) {
-      this.clothingFormData.category = category;
-    },
-    submitClothing: function(e) {
-      e.preventDefault();
-      store.dispatch("addClothingItem", this.reformatObject);
-      var emptyData = {
-        id: "",
-        category: "",
-        name: "",
-        hearts: 0,
-        clothingStyles: [],
-        ratings: {},
-        tags: [],
-        customizable: false,
-        customizableItems: []
-      };
-      this.clothingFormData = emptyData;
-      this.updateCustomItems(emptyData.customizableItems);
-      this.updateRatings(emptyData.ratings);
-      this.updateTags(emptyData.tags);
-      this.updateCustomizable(emptyData.customizable);
-      this.updateCategory(emptyData.category);
-      this.updateStyleArray(emptyData.clothingStyles);
     }
   }
 }).$mount("#nikki");
